23266fb1a6ecc75689942dd6d7bf47fcd8412a88
たくしみずき <takushimizuki@takushimizukinoMacBook-Air.local>
2025-08-12 19:17:01 +0900

    レスポンシブ対応

--- js/main.js
+++ js/main.js
@@ -120,6 +120,31 @@ $(function () {
     $(window).on('scroll', showOnScroll);
   });
 
+ /*=================================================
+    スリックのレスポンシブ化
+    ===================================================*/
+    $('.slick-area').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  dots: true,
+  arrows: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+
 });
 
 
